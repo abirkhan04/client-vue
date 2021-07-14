fd16e852939aa63cc2f910594413cc3905501701
abirkhan04 <khanabir8536@gmail.com>
2021-07-14 21:36:33 +0600

    [] client

---
+++ client/bcae1dd.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{304:function(n,t,e){"use strict";e.r(t);var o={layout:"default",components:{}},l=e(48),component=Object(l.a)(o,(function(){var n=this.$createElement;return(this._self._c||n)("div",[this._v("\n      How it works.\n ")])}),[],!1,null,null,null);t.default=component.exports}}]);
